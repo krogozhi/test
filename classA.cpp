ea9c231be5327ba183a8debc76db37b27e16d914
krogozhi <kirill.rogozhin@intel.com>
2022-03-30 17:26:45 +0300

    Create classA.cpp

---
+++ classA.cpp
@@ -0,0 +1,17 @@
+class A
+{
+	int* x;  size_t size;
+    public:
+	 A(size_t n): size(n) { x = new int[n]; }
+	 A() { size=10; x = new int[10]; }
+	 ~A() { delete x; }
+};
+
+void foo (A a);
+
+int main()
+{
+	A a;
+	foo (a);
+	return 0;
+}  
